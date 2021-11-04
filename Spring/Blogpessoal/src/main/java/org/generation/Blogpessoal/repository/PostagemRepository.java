ef3b0103179e36de997aea7a8a4f935216043ea8
waskhalifa <washingtonbrochardt@hotmail.com>
2021-11-04 12:23:06 -0300

    @WashingtonBrochardt [Task: CREATE BLOG PESSOAL]

---
+++ Spring/Blogpessoal/src/main/java/org/generation/Blogpessoal/repository/PostagemRepository.java
@@ -0,0 +1,17 @@
+package org.generation.Blogpessoal.repository;
+
+import java.util.List;
+
+import org.generation.Blogpessoal.model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem,Long>{
+	public List <Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+	
+	
+
+}
